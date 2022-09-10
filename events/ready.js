1340d05b902b9425bd190da32139c4d4faf2ee72
Dhwty <Dhwty@users.noreply.github.com>
2022-09-09 23:00:38 -0600

    Add party cron. Uses date and time specified in config.js

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,27 @@
 const { ActivityType } = require('discord.js');
 const momentTZ = require('moment-timezone');
-const { moTZ, channelAnnounce, partyName, partyDate } = require('../config.json');
+const Cron = require("croner");
+const { moTZ, channelAnnounce, channelPartyhard, partyName, partyDate } = require('../config.json');
 
 //Set party time
 const parts = partyDate.split(',');
-const partyTime = new Date(parts[0], parts[1] - 1, parts[2], parts[3], parts[4], parts[5]);
+//const partyTime = new Date(Date.now() + (5 * 60 * 1000),);
+const partyTime = new Date(parts[0], parts[1]-1, parts[2], parts[3], parts[4], parts[5]);
+
+// Begin scheduling system
+const day = (24 * 60 * 60 * 1000);
+const hour = (60 * 60 * 1000);
+const hh = (30 * 60 * 1000);
+const qh = (15 * 60 * 1000);
+const tm = (10 * 60 * 1000);
+const fm = (5 * 60 * 1000);
+
+const hourThree = new Date(partyTime.getTime() + hour);
+const hourTwo = new Date(hourThree.getTime() + hour);
+const hourOne = new Date(hourTwo.getTime() + hour);
+const minute30 = new Date(hourOne.getTime() + hh);
+const minute5 = new Date(minute30.getTime() + (hh - fm));
+const endTime = new Date(minute5.getTime() + (fm));
 
 module.exports = {
 	name: 'ready',
@@ -15,6 +32,15 @@ module.exports = {
 		// Announce party time
 		console.log('Next party @ ' + momentTZ.tz(partyTime, moTZ).toString());
 		client.channels.cache.get(channelAnnounce).setTopic('Next party @ ' + momentTZ.tz(partyTime, moTZ).toString());
+
+		//Configure cron for party
+		const hr4 = Cron( partyTime, () => {client.channels.cache.get(channelPartyhard).send( partyName + ' begins!');});
+		const hR3 = Cron( hourThree, () => {client.channels.cache.get(channelPartyhard).send('Three hours remain in the current ' + partyName + '!');});
+		const hR2 = Cron( hourTwo, () => {client.channels.cache.get(channelPartyhard).send('Two hours remain in the current ' + partyName + '!');});
+		const hR1 = Cron( hourOne, () => {client.channels.cache.get(channelPartyhard).send('One hour remains in the current ' + partyName + '!');});
+		const mR30 = Cron( minute30, () => {client.channels.cache.get(channelPartyhard).send('Thirty minutes remain in the current ' + partyName + '!');});
+		const mR5 = Cron( minute5, () => {client.channels.cache.get(channelPartyhard).send('Five minutes remain in the current ' + partyName + '!');});
+
 		//Generate and set status
 		const statNum = Math.floor(Math.random() * 6);
 		switch (statNum) {
